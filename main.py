e93c40b2d0ab4eb49915ec24991ed99de4abc1a3
SuperMorisl <pinnoymaurice2@outlook.com>
2026-01-19 23:29:34 +0100

    getting news on coingecko

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import feedparser
 import pandas as pd
 from datetime import datetime
+import requests
 
-def get_cryptonew_google(coin="bitcoin"):
-    # URL RSS officielle de Google News pour le mot-clé
+def google_news(coin="bitcoin"):
     url = f"https://news.google.com/rss/search?q={coin}+when:24h&hl=en-US&gl=US&ceid=US:en"
     
-    # On "parse" le flux
     feed = feedparser.parse(url)
     
     news_list = []
@@ -19,15 +18,46 @@ def get_cryptonew_google(coin="bitcoin"):
             "source": entry.source.title if hasattr(entry, 'source') else "Inconnue"
         })
     
+    df = pd.DataFrame(news_list)
+    df.to_csv('google.csv', index=False)
     return news_list
 
 
+def coingacko_news():
+    url = f"https://api.coingecko.com/api/v3/search/trending"
+    headers = {
+            "accept": "application/json"
+    }
+    response = requests.get(url, headers = headers)
+    if response.status_code == 200:
+         # Convert the JSON response to a Python dictionary
+         data = response.json()
+
+         # Extract relevant fields from the JSON data (coins section)
+         coin_data = []
+         for coin in data.get('coins', []):
+            coin_item = coin.get('item', {})
+            coin_data.append({
+            'id': coin_item.get('id', ''),
+            'name': coin_item.get('name', ''),
+            'symbol': coin_item.get('symbol', ''),
+            'market_cap_rank': coin_item.get('market_cap_rank', 0),
+            'score': coin_item.get('score', 0)
+        })
+
+         df = pd.DataFrame(coin_data)
+         df.to_csv('coingecko.csv', index = False)
+
+    else:
+
+         print(f"API request failed with status code: {response.status_code}")
 
 
 
-# Test du script
-articles = get_crypto_news_rss("bitcoin")
 
+# Test script
+articles = google_news("bitcoin")
+coingacko_news()
 if not articles:
     print("Toujours rien... Vérifie ta connexion internet.")
 else:
